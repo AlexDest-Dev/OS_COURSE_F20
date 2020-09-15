7b2f529475f3eedcb44b4486cf0675c8be586a2d
AlexDest-Dev <alex.rakov.n@gmail.com>
2020-09-15 20:36:56 +0300

    Week 5 added

--- week5/ex2.sh
+++ week5/ex2.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 #The race condition appears from starting of writing in file.
 #So, file with
-finishnumber=40
-while true;
+rm ex2.txt.lock;
+for i in {1..10}
 do
-    
     if ln ex2.txt ex2.txt.lock
     then
-        number=$(cat ex2.txt | tail -n 1)
-        if [ "$number" -eq "$finishnumber" ] 
-        then 
-            break 
-        fi
-        sleep 1
-    else 
-        number=$(cat ex2.txt | tail -n 1)
-        if [ "$number" -eq "$finishnumber" ] 
-        then 
-            break 
-        fi
-        expr $number + 1 >> ex2.txt
-        rm ex2.txt.lock
+        last=$(cat ex2.txt | tail -n 1);
+        expr $last + 1 >> ex2.txt;
+        rm ex2.txt.lock;
+    else
+        while ! ln ex2.txt ex2.txt.lock
+        do  
+            temp=0
+        done
+        last=$(cat ex2.txt | tail -n 1);
+        expr $last + 1 >> ex2.txt;
+        rm ex2.txt.lock;
     fi
 done
-rm ex2.txt.lock
-
+rm ex2.txt.lock;
